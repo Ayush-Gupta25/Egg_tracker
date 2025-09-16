d56f1e0d8a6811a583cabc014418c6101996b933
Ayush-Gupta25 <ayushg2500@gmail.com>
2025-09-16 16:06:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,4 +81,7 @@ def manage():
     return render_template('manage.html', entries=sorted_entries)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
